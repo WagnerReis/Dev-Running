46f739b71555b3bc943f9392f54359d267c57aed
WagnerReis <wagnerreis1997@gmail.com>
2020-03-06 19:45:39 -0300

    Iniciando o Projeto

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import jwtDecode from 'jwt-decode'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+class App extends React.Component {
+  async componentDidMount() {
+    const token = localStorage.getItem('token')
+    if (!token) {
+      const login = await axios.post('http://localhost:3001/users/login', {
+        email: 'wagner@gmail.com',
+        passwd: 'abc123'
+      })
+      const token = login.data.token
+      localStorage.setItem('token', token)
+    }
+    const decoded = jwtDecode(token)
+    console.log(decoded)
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
         </a>
-      </header>
-    </div>
-  );
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
