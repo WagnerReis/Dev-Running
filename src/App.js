c300f12cffa22011864f0b020106bf5891d7a73f
WagnerReis <wagnerreis1997@gmail.com>
2020-03-07 10:29:49 -0300

    Adicionando Reducers Sagas Reduxsauce

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import axios from 'axios'
 import jwtDecode from 'jwt-decode'
 
+import Header from './Header'
+
+import store from './redux'
+import { Provider } from 'react-redux'
+
 class App extends React.Component {
-  async componentDidMount() {
+  /*async componentDidMount() {
     const token = localStorage.getItem('token')
     if (!token) {
       const login = await axios.post('http://localhost:3001/users/login', {
@@ -23,25 +27,14 @@ class App extends React.Component {
         Authorization: 'Bearer '+token
       }
     })
-  }
+  }*/
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-        </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-        </a>
-        </header>
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <Header />
+        </div>
+      </Provider>
     );
   }
 }
