68009f6860b7e23c118b3d881b3b8f1546e95c54
Wagner Reis <wagnerreis1997@gmail.com>
2020-03-10 15:21:59 -0300

    Login controle auth

--- src/App.js
+++ src/App.js
@@ -18,25 +18,6 @@ import Restrito from './screens/Restrito'
 import Login from './screens/Login'
 
 class App extends Component {
-  /*async componentDidMount() {
-    const token = localStorage.getItem('token')
-    if (!token) {
-      const login = await axios.post('http://localhost:3001/users/login', {
-        email: 'wagner@gmail.com',
-        passwd: 'abc123'
-      })
-      const token = login.data.token
-      localStorage.setItem('token', token)
-    }
-    const decoded = jwtDecode(token)
-    console.log(decoded)
-
-    const user = await axios.get('http://localhost:3001/users/me', {
-      headers: {
-        Authorization: 'Bearer '+token
-      }
-    })
-  }*/
   render() {
     return (
       <Provider store={store}>
--- src/screens/Admin/index.js
+++ src/screens/Admin/index.js
@@ -1,21 +1,35 @@
 import React from 'react'
-import { Route, Link } from 'react-router-dom'
+import { Route, Link, Redirect } from 'react-router-dom'
+import { connect } from 'react-redux'
 
 const Home = props => <h1>Home admin</h1>
 const Users = props => <h1>Users admin</h1>
 
 const Admin = props => {
+    if (!props.auth.isAuth) {
+        return <Redirect to='/login' />
+    }
+    if(props.auth.user.role !== 'admin') {
+        return <Redirect to='/restrito' />
+    }
     return (
         <div>
             <h1>Admin</h1>
             <p>
+                {JSON.stringify(props.auth)}
                 <Link to='/admin'>Home</Link>
                 <Link to='/admin/users'>Users</Link>
             </p>
-                <Route path={`${props.match.path}/`} exact component={Home} />
-                <Route path={`${props.match.path}/users`} component={Users} />
+            <Route path={`${props.match.path}/`} exact component={Home} />
+            <Route path={`${props.match.path}/users`} component={Users} />
         </div>
     )
 }
 
-export default Admin
+const mapStateToProps = state => {
+    return {
+        auth: state.auth
+    }
+}
+
+export default connect(mapStateToProps)(Admin)
